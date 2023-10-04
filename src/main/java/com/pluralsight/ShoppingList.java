7705c10e21728f373ac83a1a4dbcbf8594b90b71
Joshua <Joshua32149@gmail.com>
2023-10-04 10:47:36 -0400

    Added items to list

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Tomatoes, Potatoes, Ribeye, Cilantro, Anchovies, Coke Zero, Eggs, Bread, Cheese, Milk");
+    }
+
+
+}
